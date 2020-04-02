81d4c5014790036fc4af59a137d5d520f37d08ae
Akira T <akira.tsuruoka.19@gmail.com>
2020-04-02 10:18:54 +0900

    created tableau dashboard

---
+++ assets/javascript/tableau.js
@@ -0,0 +1,33 @@
+let viz;
+$(".tableauGraph").on("click", function() {
+  const vizList = [
+    "http://public.tableau.com/views/RegionalSampleWorkbook/Flights",
+    "http://public.tableau.com/views/RegionalSampleWorkbook/Obesity",
+    "http://public.tableau.com/views/RegionalSampleWorkbook/College"
+  ];
+  const containerDiv = document.getElementById("vizContainer");
+  const graphId = $(this).attr("id");
+  let url;
+
+  switch (graphId) {
+    case "btn1":
+      url = vizList[0];
+      break;
+    case "btn2":
+      url = vizList[1];
+      break;
+    case "btn3":
+      url = vizList[2];
+      break;
+    default:
+      url = vizList[0];
+      break;
+  }
+
+  if (viz) {
+    // If a viz object exists, delete it.
+    viz.dispose();
+  }
+
+  viz = new tableau.Viz(containerDiv, url);
+});
